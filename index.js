4171e6e3e1f8e18a85d151cdc2c41e37536aa437
Nazzmus sakib <nazzmussakib990@gmail.com>
2023-05-02 19:05:12 +0600

    chefs-data has been created

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
+const chefsData = require("./data/chefsData.json");
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-app.get("");
+app.get("/chefs-data", (req, res) => {
+  res.send(chefsData);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
