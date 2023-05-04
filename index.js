4f138688b2c774525497ef5955f0c2a3ca1bcfbe
Nazzmus sakib <nazzmussakib990@gmail.com>
2023-05-04 12:18:40 +0600

    recipie data added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+var cors = require("cors");
+app.use(cors());
 const chefsData = require("./data/chefsData.json");
 app.get("/", (req, res) => {
   res.send("Hello World!");
