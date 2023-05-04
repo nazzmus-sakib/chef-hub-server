e5f4c722ff2222214070f32ed5198880a172f3ac
Nazzmus sakib <nazzmussakib990@gmail.com>
2023-05-04 13:38:49 +0600

    data ready for each chef with id

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ app.get("/", (req, res) => {
 app.get("/chefs-data", (req, res) => {
   res.send(chefsData);
 });
+app.get("/chefs-data/:id", (req, res) => {
+  const id = req.params.id;
+  const chefData = chefsData.find((data) => data.id === id);
+  res.send(chefData);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
